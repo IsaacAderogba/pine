c19d091daa1179a3d03b2a824750cc8189913a7b
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-08-05 08:00:37 +0100

    Add event emitter

---
+++ src/core/utils/EventEmitter.ts
@@ -0,0 +1,30 @@
+type KeyOf<T> = Extract<keyof T, string>;
+
+interface Events {
+  [key: string]: (...args: any) => void;
+}
+
+export type EventApi<T extends Events> = T;
+
+export class EventEmitter<T extends Events> {
+  private listeners: { [name: string]: Set<Function> } = {};
+
+  public on<E extends KeyOf<T>>(event: E, cb: T[E]) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = new Set();
+    }
+
+    this.listeners[event].add(cb);
+    return () => {
+      this.listeners[event]?.delete(cb);
+    };
+  }
+
+  protected emit<E extends KeyOf<T>>(event: E, ...args: Parameters<T[E]>) {
+    this.listeners[event]?.forEach(cb => cb.apply(this, args));
+  }
+
+  protected destroyListeners(): void {
+    this.listeners = {};
+  }
+}
